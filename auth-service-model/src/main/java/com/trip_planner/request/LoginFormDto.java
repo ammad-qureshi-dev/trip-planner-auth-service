2d260e6742b5e187c69006ea146435b81bd33a7e
ammad-qureshi-dev <ammadq87@my.yorku.ca>
2025-07-05 16:58:06 -0400

    test: fix build

--- auth-service-model/src/main/java/com/trip_planner/request/LoginFormDto.java
+++ auth-service-model/src/main/java/com/trip_planner/request/LoginFormDto.java
@@ -6,7 +6,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginFormDto {
-    @NonNull private String email;
+  @NonNull private String email;
 
   @NonNull private String password;
 }
